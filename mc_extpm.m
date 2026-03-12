6add73e1a4c5ce2257ff76947d659f50e79d6bc4
Raonaqul Islam <119525884+raonaqul-islam@users.noreply.github.com>
2026-03-12 13:02:33 -0400

    Update mc_extpm.m

--- mc_extpm.m
+++ mc_extpm.m
@@ -1,4 +1,4 @@
-%% MONTE-CARLO SIMULATION FOR EXTERNAL PHASE-MODULATION OF PUMP
+% MONTE-CARLO SIMULATION FOR EXTERNAL PHASE-MODULATION OF PUMP
 % This routine simulates when there is noise in the system that adds up in
 % every time step, but there is also external phase modulation using
 % external RF source.
@@ -6,7 +6,7 @@
 % ---------------------------------+
 % Raonaqul Islam, UMBC             |
 % Date started: March 19, 2025     |
-% Last updated: September 02, 2025 |
+% Last updated: September 16, 2025 |
 % ---------------------------------+
 
 clear
@@ -15,12 +15,12 @@ close all
 tic % Starts counting simulation time
 
 % Initial condition from previous solution
-load ./data/intextpm2.mat
+load ./data/intextpm100ghz_3.mat
     
 % Monte-Carlo parameters
-N_sim        = 300;                    % No. of Monte-Carlo simulations
-N_steps      = 5e6;                    % No. of steps in each iteration
-extpmtheta0  = zeros(N_steps,N_sim);   % Store peaks
+N_sim        = 120;                    % No. of Monte-Carlo simulations
+N_steps      = 1e8;                    % No. of steps in each iteration
+savestep     = 1;                      % Save every "S" steps to reduce memory usage
 
 parfor k = 1:N_sim
 
@@ -33,11 +33,12 @@ parfor k = 1:N_sim
     %                        SIMULATION PARAMETERS                        %
     % *********************************************************************
     N           = 512;                             % No. of points on axis | Spatial resolution
-    dt          = 1e-4;                            % Temporal step size
+    dt          = 1e-2;                            % Temporal step size
     psi_0       = psi_prev;                        % Initial condition from previous solution
-    theta0     = zeros(N_steps,1);                % Initiate peak position array
-    circmean   = zeros(N_steps,1);                % Initiate circular mean array
-
+    theta0      = zeros(N_steps/savestep,1);       % Initiate peak position array
+    circmean    = zeros(N_steps/savestep,1);       % Initiate circular mean array
+    dw_rep = zeros(N_steps,1);
+    count       = 1;                               % Sets the index of saving-to-file
 
     % *********************************************************************
     %                          NOISE PARAMETERS                           %
@@ -51,13 +52,13 @@ parfor k = 1:N_sim
         % *****************************************************************
         %                           PARAMETERS                            %
         % *****************************************************************
-        alpha   = 2.0;                            % Detuning
+        alpha   = 3.2;                            % Detuning
         beta    = -0.2;                           % Dispersion, beta2 only in this case
-        F       = 1.4103;                         % Pump amplitude
+        F       = 2.2;                            % Pump amplitude
         gamma   = 1.0;                            % Normalized nonlinear coefficient
-        deltaM  = 0.65;                           % Phase modulation   
-        gamma_n = 2e5;                            % Noise disspation factor
-        tph     = 2.5e-6;                         % Photon lifetime
+        deltaM  = 0.65;                            % Phase modulation   
+        gamma_n = 1e6;                            % Noise disspation factor
+        tph     = 8.23e-10;                       % Photon lifetime
 
         % *****************************************************************
         %                         DISCRETIZATION                          %
@@ -98,20 +99,30 @@ parfor k = 1:N_sim
         % Output after one-step
         psi_out     = psi_nl2;
 
-        % Calculate centroid        
-        I           = abs(psi_out).^2;               % Intensity
-        theta0(ix)  = sum(I.*theta)/sum(I);          % Center of mass
-        circmean(ix) = angle(sum(I.*exp(1i*theta))); % Weighted circular mean
+        % Calculate centroid  
+        if rem(ix,savestep)==0
+            I               = abs(psi_out).^2;               % Intensity
+            theta0(count)   = sum(I.*theta)/sum(I);          % Centroid
+            circmean(count) = angle(sum(I.*exp(1i*theta)));  % Weighted circular mean
+            if ix>1
+                dw_rep(count) = (theta0(count)-theta0(count-1))./dt;
+            end
+            count           = count+1;                       % Index count
+        end
 
     end
     
-    % Store this iteration
-    extpmtheta0(:,k)   = theta0;
-    extpmcircmean(:,k) = circmean;
+    % *********************************************************************
+    %                             SAVE DATA                               %
+    % *********************************************************************        
+    % Form a structure to store data (because of the parallel pool)
+    data_structure = struct('theta0',theta0,'circmean',circmean);                           
+    % Save the structure   
+    save(sprintf('./data/mc_extpm_092825/mc_extpm1_092825_%d.mat',k),'-fromstruct',data_structure); 
 
 end
 
 time_elapsed = toc;
 
-% Save the results in a mat file (v7.3 for larger files)
-save('./data/mc_extpm_020925.mat','extpmtheta0','extpmcircmean','time_elapsed','-v7.3');
+% Show elapsed time
+disp(['Elapsed time = ' num2str(time_elapsed)]);
